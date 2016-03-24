08462fad9f2e6ef9ce2e9375b1a9fcbeb31d1ec5
Bastien Calou <bastien.calou@gmail.com>
2016-03-24 12:28:32 +0100

    [forms] add a model-controlled form

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,7 +1,31 @@
 import {Component} from 'angular2/core';
+import {ControlGroup, Control, FormBuilder, Validators} from 'angular2/common';
+import {hpValidator} from '../validators/hpValidator';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/templates/app.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  pokemonForm: ControlGroup;
+
+  constructor(private _fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.pokemonForm = this._fb.group({
+      "name": ['', Validators.required],
+      "description": ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(20)
+      ])],
+      "evolution": [''], // no validator but allow us to get the value in the end
+      "hp": ['', hpValidator]
+    });
+  }
+
+  submitPokemon() {
+    console.log(this.pokemonForm.value);
+  }
+}
