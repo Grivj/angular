86626a0cfa2d3a41aff9b5d7aa8ff594043269b2
Bastien Calou <bastien.calou@gmail.com>
2016-03-24 09:34:33 +0100

    [async] use async pipe to handle names list

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {NamesService} from '../services/names.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
     selector: 'my-app',
@@ -7,43 +8,13 @@ import {NamesService} from '../services/names.service';
     providers: [NamesService]
 })
 export class AppComponent {
-  names: Array<Object>;
-  projects: Array<Object>;
+  names: Observable<Object[]>;
 
   constructor(private _namesService: NamesService) {
   }
 
   // Single request example
   getNames() {
-    this._namesService.getNames().subscribe(
-      (names) => {
-        console.log(names);
-        this.names = names;
-      },
-      (err) => {
-        console.warn(err)
-      },
-      () => {
-        console.log("Names request done")
-      }
-    );
-  }
-
-  // Multiple requests example
-  getNamesAndProjects() {
-    this._namesService.getNamesAndProjects().subscribe(
-      (data) => {
-        console.log("names : ", data[0]);
-        this.names = data[0];
-        console.log("projects : ", data[1]);
-        this.projects = data[1];
-      },
-      (err) => {
-        console.warn(err);
-      },
-      () => {
-        console.log("Names and projects requests done")
-      }
-    );
+    this.names = this._namesService.getNames();
   }
 }
